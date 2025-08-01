d95d110f4c5b015f4e92ea5d967479cab3de6580
Noemi Baglieri <105011724+noemibaglieri@users.noreply.github.com>
2025-08-01 16:20:26 +0200

    final changes

--- src/main/java/noemibaglieri/controllers/AuthController.java
+++ src/main/java/noemibaglieri/controllers/AuthController.java
@@ -10,7 +10,6 @@ import noemibaglieri.services.AuthService;
 import noemibaglieri.services.UsersService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
-import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.validation.BindingResult;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
--- src/main/java/noemibaglieri/exceptions/BadEnumException.java
+++
@@ -1,7 +0,0 @@
-package noemibaglieri.exceptions;
-
-public class BadEnumException extends RuntimeException {
-    public BadEnumException(String message) {
-        super(message);
-    }
-}
