7f7f28debfe395bdfa25b6d3be3449bb1ea17e68
Noemi Baglieri <105011724+noemibaglieri@users.noreply.github.com>
2025-08-01 09:26:41 +0200

    Project set-up. application.properties. jwt dependencies

---
+++ src/test/java/noemibaglieri/U5W3D5ApplicationTests.java
@@ -0,0 +1,13 @@
+package noemibaglieri;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class U5W3D5ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
